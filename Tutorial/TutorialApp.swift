c1d246a9b0127c32fe81de16b3128ff5a4edf7a1
mfikrirmdhn <mfikrirmdhn21@gmail.com>
2026-08-29 18:45:04 +0700

    Initial Commit

---
+++ Tutorial/TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialApp.swift
+//  Tutorial
+//
+//  Created by zxura on 29/08/26.
+//
+
+import SwiftUI
+
+@main
+struct TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
